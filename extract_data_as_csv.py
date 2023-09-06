b3c60b88161dfbec434a417158f18f1c263834cf
Failey <m.failey@fugro.com>
2023-09-06 16:28:48 +0100

    loop through bad records

--- extract_data_as_csv.py
+++ extract_data_as_csv.py
@@ -20,8 +20,7 @@ print(full_df.columns.values)
 
 """
 for each invalid line:
-    if prev line within half an hour
-        get rainfall every ten minutes over prev 2 hours
+    get rainfall every ten minutes over prev 2 hours
     get rainfall for every ten mins until time of next valid record
     get rainfall for every ten mins for following hour
 
@@ -30,4 +29,14 @@ for each invalid line:
 """
 
 for i, row in bad_df.iterrows():
+    #get record of bad line, its date, time and rain accumulation value
     bad_rec = row["invalid"]
+    br_date = bad_rec[0:10]
+    br_time = bad_rec[11:19]
+    rain_accum = bad_rec[bad_rec.find("Rc")+3:bad_rec.find(",",bad_rec.find("Rc"))-1]
+
+    #round minute to nearest ten minutes
+    br_time = br_time[:14] + str(round(int(br_time[14:16]), -1)) + br_time[17:]
+
+    #look for matching date in full_df
+
